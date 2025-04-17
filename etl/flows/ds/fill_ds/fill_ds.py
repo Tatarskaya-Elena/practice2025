3bd01668126097b49915f27fc9e6d6fbe83734a9
tatarskaya-elena <tatarskaya.elena@mail.ru>
2025-04-17 12:50:33 +0300

    add and fill tables (without fill md_ledger_account_s)

--- etl/flows/ds/fill_ds/fill_ds.py
+++ etl/flows/ds/fill_ds/fill_ds.py
@@ -22,6 +22,80 @@ def main():
 
     ft_balance_f.write.format("hive").mode("append").saveAsTable("ds.ft_balance_f")
 
+    ft_posting_f = spark.read.option("delimiter", ";").csv(os.path.join(DATA_PATH, "ft_posting_f.csv"), header=True)
+    ft_posting_f = ft_posting_f \
+        .withColumn("oper_date", to_date(col("oper_date"))) \
+        .withColumn("credit_account_rk", col("credit_account_rk").cast("decimal(38, 10)")) \
+        .withColumn("debet_account_rk", col("debet_account_rk").cast("decimal(38, 10)")) \
+        .withColumn("credit_amount", col("credit_amount").cast("float")) \
+        .withColumn("debet_amount", col("debet_amount").cast("float")) \
 
+    ft_posting_f.write.format("hive").mode("append").saveAsTable("ds.ft_posting_f")
+    
+    md_account_d= spark.read.option("delimiter", ";").csv(os.path.join(DATA_PATH, "md_account_d.csv"), header=True)
+    md_account_d = md_account_d \
+        .withColumn("data_actual_date", to_date(col("data_actual_date"))) \
+        .withColumn("data_actual_end_date", to_date(col("data_actual_end_date"))) \
+        .withColumn("account_rk", col("account_rk").cast("decimal(38, 10)")) \
+        .withColumn("account_number", col("account_number").cast("string")) \
+        .withColumn("char_type", col("char_type").cast("string")) \
+        .withColumn("currency_rk", col("currency_rk").cast("decimal(38, 10)")) \
+        .withColumn("currency_code", col("currency_code").cast("string")) \
+
+    md_account_d.write.format("hive").mode("append").saveAsTable("ds.md_account_d")
+
+    md_currency_d= spark.read.option("delimiter", ";").csv(os.path.join(DATA_PATH, "md_currency_d.csv"), header=True)
+    md_currency_d = md_currency_d \
+        .withColumn("currency_rk", col("currency_rk").cast("decimal(38, 10)")) \
+        .withColumn("data_actual_date", to_date(col("data_actual_date"))) \
+        .withColumn("data_actual_end_date", to_date(col("data_actual_end_date"))) \
+        .withColumn("currency_code", col("currency_code").cast("string")) \
+        .withColumn("code_iso_char", col("code_iso_char").cast("string")) \
+
+    md_currency_d.write.format("hive").mode("append").saveAsTable("ds.md_currency_d")
+
+    md_exchange_rate_d= spark.read.option("delimiter", ";").csv(os.path.join(DATA_PATH, "md_exchange_rate_d.csv"), header=True)
+    md_exchange_rate_d = md_exchange_rate_d \
+        .withColumn("data_actual_date", to_date(col("data_actual_date"))) \
+        .withColumn("data_actual_end_date", to_date(col("data_actual_end_date"))) \
+        .withColumn("currency_rk", col("currency_rk").cast("decimal(38, 10)")) \
+        .withColumn("reduced_cource", col("reduced_cource").cast("float")) \
+        .withColumn("code_iso_num", col("code_iso_num").cast("string")) \
+
+    md_exchange_rate_d.write.format("hive").mode("append").saveAsTable("ds.md_exchange_rate_d")
+
+"""   md_ledger_account_s= spark.read.option("delimiter", ";").csv(os.path.join(DATA_PATH, "md_ledger_account_s.csv"), header=True)
+    md_ledger_account_s= md_ledger_account_s \
+        .withColumn("chapter", col("chapter").cast("string")) \
+        .withColumn("chapter_name", col("chapter_name").cast("string")) \
+        .withColumn("section_number", col("section_number").cast("int")) \
+        .withColumn("section_name", col("section_name").cast("string")) \
+        .withColumn("subsection_name", col("subsection_name").cast("string")) \
+        .withColumn("ledger1_account", col("ledger1_account").cast("int")) \
+        .withColumn("ledger1_account_name", col("ledger1_account_name").cast("string")) \
+        .withColumn("ledger_account", col("ledger_account").cast("int")) \
+        .withColumn("ledger_account_name", col("ledger_account_name").cast("string")) \
+        .withColumn("characteristic", col("characteristic").cast("int")) \
+        .withColumn("is_resident", col("is_resident").cast("int")) \
+        .withColumn("is_reserve", col("is_reserve").cast("int")) \
+        .withColumn("is_reserved", col("is_reserved").cast("int")) \
+        .withColumn("is_loan", col("is_loan").cast("int")) \
+        .withColumn("is_reserved_assets", col("is_reserved_assets").cast("int")) \
+        .withColumn("is_overdue", col("is_overdue").cast("int")) \
+        .withColumn("is_interest", col("is_interest").cast("int")) \
+        .withColumn("pair_account", col("pair_account").cast("string")) \
+        .withColumn("start_date", to_date(col("start_date"))) \
+        .withColumn("end_date", to_date(col("end_date"))) \
+        .withColumn("is_rub_only", col("is_rub_only").cast("int")) \
+        .withColumn("currency_code", col("currency_code").cast("string")) \
+        .withColumn("min_term", col("min_term").cast("string")) \
+        .withColumn("min_term_measure", col("min_term_measure").cast("string")) \
+        .withColumn("max_term", col("max_term").cast("string")) \
+        .withColumn("max_term_measure", col("max_term_measure").cast("string")) \
+        .withColumn("ledger_acc_full_name_translit", col("ledger_acc_full_name_translit").cast("string")) \
+        .withColumn("is_revaluation", col("is_revaluation").cast("string")) \
+        .withColumn("is_correct", col("is_correct").cast("string"))
+    md_ledger_account_s.write.format("hive").mode("append").saveAsTable("ds.md_ledger_account_s")
+"""
 if __name__ == "__main__":
     main()
--- etl/flows/ds/fill_ds/sql/create_ds_tables.sql
+++ etl/flows/ds/fill_ds/sql/create_ds_tables.sql
@@ -4,3 +4,64 @@ CREATE TABLE IF NOT EXISTS ds.ft_balance_f (
     currency_rk DECIMAL(38, 0),
     balance_out FLOAT
 ) STORED AS PARQUET;
+CREATE TABLE IF NOT EXISTS ds.ft_posting_f (
+    oper_date DATE,
+    credit_account_rk DECIMAL(38, 0),
+    debet_account_rk DECIMAL(38, 0),
+    credit_amount FLOAT,
+    debet_amount FLOAT
+) STORED AS PARQUET;
+CREATE TABLE IF NOT EXISTS ds.md_account_d (
+    data_actual_date DATE,
+    data_actual_end_date DATE,
+    account_rk DECIMAL,
+    account_number STRING,
+    char_type STRING,
+    currency_rk DECIMAL,
+    currency_code STRING
+) STORED AS PARQUET;
+CREATE TABLE IF NOT EXISTS ds.md_currency_d (
+    currency_rk DECIMAL,
+    data_actual_date DATE,
+    data_actual_end_date DATE,
+    currency_code STRING,
+    code_iso_char STRING
+) STORED AS PARQUET;
+CREATE TABLE IF NOT EXISTS ds.md_exchange_rate_d (
+    data_actual_date DATE,
+    data_actual_end_date DATE,
+    currency_rk DECIMAL,
+    reduced_cource FLOAT,
+    code_iso_num STRING
+) STORED AS PARQUET;
+CREATE TABLE IF NOT EXISTS ds.md_ledger_account_s (
+    chapter STRING,
+    chapter_name STRING,
+    section_number INT,
+    section_name STRING,
+    subsection_name STRING,
+    ledger1_account INT,
+    ledger1_account_name STRING,
+    ledger_account INT,
+    ledger_account_name STRING,
+    characteristic INT,
+    is_resident INT,
+    is_reserve INT,
+    is_reserved INT,
+    is_loan INT,
+    is_reserved_assets INT,
+    is_overdue INT,
+    is_interest INT,
+    pair_account STRING,
+    start_date DATE,
+    end_date DATE,
+    is_rub_only INT,
+    currency_code STRING,
+    min_term STRING,
+    min_term_measure STRING,
+    max_term STRING,
+    max_term_measure STRING,
+    ledger_acc_full_name_translit STRING,
+    is_revaluation STRING,
+    is_correct STRING
+) STORED AS PARQUET;
