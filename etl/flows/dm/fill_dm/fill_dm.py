a34f210e7a7765f497ae1b802391baf06191bb2a
tatarskaya-elena <tatarskaya.elena@mail.ru>
2025-04-19 18:45:39 +0300

    create showcase

--- etl/flows/dm/fill_dm/fill_dm.py
+++ etl/flows/dm/fill_dm/fill_dm.py
@@ -7,9 +7,10 @@ from pyspark.sql.functions import *
 
 SQL_PATH = os.path.join(os.path.dirname(__file__), "sql")
 
+
+
 def main():
-    spark = get_spark_session(app_name="fill_ds")
-    spark.sparkContext.setCheckpointDir(os.path.join(SQL_PATH,"tmp"))
+    spark = get_spark_session(app_name="fill_dm")
 
     sql_from_file(spark=spark, file_path=os.path.join(SQL_PATH, "create_showcase.sql"), log_sql=True)
 
