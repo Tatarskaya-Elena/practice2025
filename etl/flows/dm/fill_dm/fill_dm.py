18ddf2f0c6ac45b263a4e2ee300f55d1db60f332
tatarskaya-elena <tatarskaya.elena@mail.ru>
2025-04-20 18:15:48 +0300

    edit fill_account_turnover_f and fill dm_account_balance_f on 31/12/2017

--- etl/flows/dm/fill_dm/fill_dm.py
+++ etl/flows/dm/fill_dm/fill_dm.py
@@ -10,10 +10,10 @@ spark = get_spark_session(app_name="fill_dm")
 spark.sparkContext.setCheckpointDir(os.path.join(SQL_PATH,"tmp"))
 
 def fill_account_turnover_f(date):
+    df = spark.table("dm.dm_account_turnover_f")
+    filtered_df = df.filter(col("on_date") != date).checkpoint()
+    filtered_df.write.mode('overwrite').saveAsTable("dm.dm_account_turnover_f")
     sql_from_file(spark=spark, file_path=os.path.join(SQL_PATH, "fill_account_turnover_f.sql"), log_sql=True, on_date=date)
-    dm_account_turnover_f=spark.read.table("dm.dm_account_turnover_f")
-    dm_account_turnover_f_unique = dm_account_turnover_f.dropDuplicates().checkpoint()
-    dm_account_turnover_f_unique.write.format("hive").mode("overwrite").insertInto("dm.dm_account_turnover_f")
 
 def main():
     sql_from_file(spark=spark, file_path=os.path.join(SQL_PATH, "create_showcase.sql"), log_sql=True)
@@ -21,5 +21,9 @@ def main():
     while current_date <= datetime(2018, 1, 31):
         fill_account_turnover_f(current_date.date())
         current_date += timedelta(days=1)
+    dm_account_balance_f = spark.table("dm.dm_account_balance_f")
+    filtered_dm_account_balance_f = dm_account_balance_f.filter(col("on_date") != '2017-12-31').checkpoint()
+    filtered_dm_account_balance_f.write.mode('overwrite').saveAsTable("dm.dm_account_balance_f")
+    sql_from_file(spark=spark, file_path=os.path.join(SQL_PATH, "insert_balance.sql"), log_sql=True)
 if __name__ == "__main__":
     main()
