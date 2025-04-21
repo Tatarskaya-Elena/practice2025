1933a99c7a53405b0f8c1be6d5ee759a000757ca
tatarskaya-elena <tatarskaya.elena@mail.ru>
2025-04-21 13:12:56 +0300

    fill_account_balance_f and log it

--- etl/flows/dm/fill_dm/fill_dm.py
+++ etl/flows/dm/fill_dm/fill_dm.py
@@ -16,7 +16,14 @@ def fill_account_turnover_f(date):
     filtered_df.write.mode('overwrite').saveAsTable("dm.dm_account_turnover_f")
     sql_from_file(spark=spark, file_path=os.path.join(SQL_PATH, "fill_account_turnover_f.sql"), log_sql=True, on_date=date)
 
+def fill_account_balance_f(date):
+    df = spark.table("dm.dm_account_balance_f")
+    filtered_df = df.filter(col("on_date") != date).checkpoint()
+    filtered_df.write.mode('overwrite').saveAsTable("dm.dm_account_balance_f")
+    sql_from_file(spark=spark, file_path=os.path.join(SQL_PATH, "fill_account_balance_f.sql"), log_sql=True, on_date=date)
+
 def main():
+
     fal=False
     start_time = datetime.now() 
     try:
@@ -30,6 +37,7 @@ def main():
     finally:
         end_time = datetime.now() 
         log_process_start_end(spark,"fill_account_turnover_f", start_time, end_time, fal)
+
     fal=False
     start_time = datetime.now() 
     try:
@@ -37,10 +45,14 @@ def main():
         filtered_dm_account_balance_f = dm_account_balance_f.filter(col("on_date") != '2017-12-31').checkpoint()
         filtered_dm_account_balance_f.write.mode('overwrite').saveAsTable("dm.dm_account_balance_f")
         sql_from_file(spark=spark, file_path=os.path.join(SQL_PATH, "insert_balance.sql"), log_sql=True)
+        current_date = datetime(2018, 1, 1)
+        while current_date <= datetime(2018, 1, 31):
+            fill_account_balance_f(current_date.date())
+            current_date += timedelta(days=1)
     except Exception as e:
         fal = True
     finally:
         end_time = datetime.now()
-    spark.sql('SELECT * FROM log.data_load_logs').show()
+        log_process_start_end(spark,"fill_account_balance_f", start_time, end_time, fal)
 if __name__ == "__main__":
     main()
